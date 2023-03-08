c7a6e979aa3bd6b5f3d4549d2d16768f9fc4177c
Vikram Waghmare <vikramw@prescient.in>
2023-03-08 09:14:57 +0530

    first commit

---
+++ ConsoleSampleCSharp/ConsoleSampleCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleSampleCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			System.Console.WriteLine("Hello World!");
+			String fullPath =  System.Environment.CurrentDirectory + "/myfile.txt";
+			using (StreamWriter writer = new StreamWriter(fullPath))
+{
+   writer.WriteLine("This is sample output file.");
+   writer.WriteLine("It is generated through C# code.");
+}
+
+        }
+    }
+}
